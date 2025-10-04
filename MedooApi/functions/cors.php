fa8298417019363e4ac0fbd8edd87668ec539c1b
Jignesh Parmar <jigneshpa@gmail.com>
2025-10-04 17:27:08 +0530

    Updated cors.php

--- MedooApi/functions/cors.php
+++ MedooApi/functions/cors.php
@@ -1,9 +1,27 @@
 <?php
 
 function handle_cors() {
-    $origin = getenv('CORS_ORIGIN') ?: 'http://localhost:5173';
+    // Allow all localhost origins
+    $allowed_origins = [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+        'http://localhost:3000',
+        'http://localhost:4173',
+        'http://127.0.0.1:5173',
+        'http://127.0.0.1:5174',
+        'http://127.0.0.1:5175'
+    ];
 
-    header("Access-Control-Allow-Origin: $origin");
+    $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+    if (in_array($origin, $allowed_origins) || getenv('CORS_ORIGIN')) {
+        $allowed_origin = getenv('CORS_ORIGIN') ?: $origin;
+        header("Access-Control-Allow-Origin: $allowed_origin");
+    } else {
+        header("Access-Control-Allow-Origin: *");
+    }
+
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header("Access-Control-Allow-Credentials: true");
